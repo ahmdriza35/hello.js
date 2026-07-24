040e7046e83732d68e7672fb28c1686d9bfcd53e
ahmdriza35 <akhmadrizal427@gmail.com>
2026-07-24 21:57:16 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,12 @@
+let skorPemain = 0; 
+let namapemain = "Riza"; 
+let sudahLulus = false; 
+let alamat;
+
+console.log(typeof namaPemain);   // "string" 
+console.log(typeof skorPemain);   // "number" 
+console.log(typeof sudahLulus);   // "boolean" 
+console.log(typeof alamat);       // "undefined" 
+
+skorPemain = 10; // let boleh diubah nilainya 
+console.log(`Halo, namaku ${namaPemain}, skorku ${skorPemain}.`); 
