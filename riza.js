8a46aa89178744cde195d4f4a5f17825a5a6ced6
ahmdriza35 <akhmadrizal427@gmail.com>
2026-07-24 21:56:35 +0700

    Add files via upload

---
+++ riza.js
@@ -0,0 +1,4 @@
+console.log("Ahmad Riza Wahyudi");
+console.log("2 April 2011");
+console.log("MTsN Kota Pasuruan");
+console.log("saya masuk SMK Telkom karena saya ingin mengembangkan kemampuan di bidang IT");
