b989e1149ffdbeba40722b70c09ccbbf06afe13e
ahmdriza35 <akhmadrizal427@gmail.com>
2026-07-24 21:56:56 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,18 @@
+//Deklarasi Variabel
+let angka = 100;        //Number
+let sekolah = "Moklet"; //String
+let kondisi = false;    //Boolean
+let data;               //undefined
+let nama = null;        //Null
+
+console.log(typeof  angka);
+console.log(typeof sekolah);
+console.log(typeof kondisi);
+console.log(typeof data);
+console.log(typeof nama);
+
+console.log(`ini adalah angka : ${angka}`);
+console.log(`ini adalah sekolah : ${sekolah}`);
+console.log(`ini adalah kondisi : ${kondisi}`);
+console.log(`ini adalah data : ${data}`);
+console.log(`ini adalah nama : ${nama}`);
